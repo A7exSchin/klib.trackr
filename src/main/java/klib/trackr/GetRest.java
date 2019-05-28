d8b9dc0e4d09149bda9cbbe76aece76223f86c54
Peter Kirschner <peter@kirschners.de>
2019-05-28 13:17:47 +0200

    fix missing bin folder in gitignore

--- src/main/java/klib/trackr/GetRest.java
+++ src/main/java/klib/trackr/GetRest.java
@@ -1,5 +1,8 @@
 package klib.trackr;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
@@ -8,21 +11,17 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
-import java.io.*;
-import java.util.LinkedHashMap;
-import java.util.Map;
-import java.util.Scanner;
 
 @SpringBootApplication
 public class GetRest {
 
 	private static final Logger log = LoggerFactory.getLogger(GetRest.class);
-	
+
 	public static String start;
 	public static String end;
-	
+
 	public static void main(String args[]) {
-		Map<String,String> arguments = new LinkedHashMap<String, String>();
+		Map<String, String> arguments = new LinkedHashMap<String, String>();
 		for (int i = 0; i < args.length; i++) {
 			String key = args[i].split("=")[0];
 			String value = args[i].split("=")[1];
@@ -40,7 +39,7 @@ public class GetRest {
 	public RestTemplate restTemplate(RestTemplateBuilder builder) {
 		return builder.build();
 	}
-	
+
 	@Bean
 	public CommandLineRunner run(RestTemplate restTemplate) throws Exception {
 		return args -> {
